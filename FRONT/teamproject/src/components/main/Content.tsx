bc84dbb8efb6de01612b3d533d55dded211ad5fc
Kim Chanwoo <rlacksdn1223@naver.com>
2025-11-04 16:34:33 +0900

    회의기록 업데이트 내일 할거 추가

--- FRONT/teamproject/src/components/main/Content.tsx
+++ FRONT/teamproject/src/components/main/Content.tsx
@@ -1,7 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function Content({ country }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get('http://localhost:8080/api/place')
+      setData(response.data.results)
+    }
+    getData();
+  }, [])
+
+
   return (
     <>
-      
+      <ul> 
+        {
+          [...data]
+            .sort((a, b) => b.rating - a.rating)
+            .map(item => (
+              <li key={item.place_id}>
+                <strong>{item.name}</strong> - 평점: {item.rating}점
+              </li>
+            ))
+        }
+      </ul>
     </>
   );
 }
