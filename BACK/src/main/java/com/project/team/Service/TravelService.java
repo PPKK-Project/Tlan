3b6fb06319d80ed07b9855a185181651f3b55c15
sun <sunyp0713@naver.com>
2025-11-10 16:19:52 +0900

    travelcontroller

--- BACK/src/main/java/com/project/team/Service/TravelService.java
+++ BACK/src/main/java/com/project/team/Service/TravelService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Principal;
 
 import java.security.Principal;
 import java.util.List;
