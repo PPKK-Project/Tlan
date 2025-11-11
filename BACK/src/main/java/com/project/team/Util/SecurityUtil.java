930fe90923a4482d97b445568673648b2c56c836
alstn9213 <alstn9213@naver.com>
2025-11-11 09:10:37 +0900

    feat: 20251111 permission

--- BACK/src/main/java/com/project/team/Util/SecurityUtil.java
+++ BACK/src/main/java/com/project/team/Util/SecurityUtil.java
@@ -4,7 +4,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.Authentication;
 
 /**
- * Spring Security 컨텍스트에서 현재 사용자의 'username'(이메일)을 가져옵니다.
+ * Spring Security 컨텍스트에서 현재 사용자의 데이터를 받아올 수 있는 메서드들을 정의할 클래스.
+ * 현재는 사용자의 Email을 받아오는 메서드만 정의함.
+ * UserService의 patchNickname메서드 매개변수인 Principal 대신 이곳의 메서드를 쓰면 매개변수가 줄어들고
+ * 코드가 간략해지며 유연성이 늘어나니, 혹시 모를 나중을 위해 이 클래스를 남겨둡니다.
+ * .
  */
 public class SecurityUtil {
 
