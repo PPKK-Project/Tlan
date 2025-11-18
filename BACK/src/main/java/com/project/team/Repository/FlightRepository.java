20cb15fd7f5e21bba9f58fe2a1d25ef2e4bde04e
Kim Chanwoo <rlacksdn1223@naver.com>
2025-11-18 12:05:05 +0900

    tt

--- BACK/src/main/java/com/project/team/Repository/FlightRepository.java
+++ BACK/src/main/java/com/project/team/Repository/FlightRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-    Optional<Flight> findByFlightId(Long flightId);
+    Optional<Flight> findByTravelId(Long travelId);
 }
