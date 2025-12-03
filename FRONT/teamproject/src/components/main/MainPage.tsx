98ae7f1b43638ce2f2b87a7c392395284f7fc932
Kim Chanwoo <rlacksdn1223@naver.com>
2025-12-03 10:30:31 +0900

    fix: 로그아웃시 채팅 안나오게 수정

--- FRONT/teamproject/src/components/main/MainPage.tsx
+++ FRONT/teamproject/src/components/main/MainPage.tsx
@@ -3,14 +3,16 @@ import Warning from './Warning';
 import Place from './Place';
 import Footer from './Footer';
 import Chat from '../chatting/Chat';
+import { useAuthSession } from '../../hooks/useAuthSession';
 
 function MainPage() {
+  const { isLogin } = useAuthSession();
   return (
     <>
       <Planning />
       <Warning />
       <Place />
-      <Chat/>
+      {isLogin ? <Chat /> : ''}
       <Footer />
     </>
   );
